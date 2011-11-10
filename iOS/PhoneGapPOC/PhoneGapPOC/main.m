e1e9d49ecbfb11a237db09b39b314b176887cb28
Ben smith <benjamint.smith@newsint.co.uk>
2011-11-10 08:01:02 +0000

    Phone Gap

---
+++ iOS/PhoneGapPOC/PhoneGapPOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PhoneGapPOC
+//
+//  Created by Ben Smith on 09/11/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
